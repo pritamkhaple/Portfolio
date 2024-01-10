68423f8a90b4c887fe138023d27fda90b2e62919
Pritam Khaple <110626364+pritamkhaple@users.noreply.github.com>
2024-01-10 19:08:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// For Hamburger navigation
+
+let burger = document.getElementById("burg");
+
+burger.addEventListener("click", () => {
+    console.log("burger clicked") //just to check
+        document.querySelector(".nav").style.display = "flex";
+        document.querySelector(".navigation").style.height = "100vh";
+        document.querySelector("#burg").style.display = "none";
+        document.querySelector(".close").style.display = "block";
+        document.querySelector("body").style.overflow = "hidden";
+});
+
+let clos = document.getElementById("closed");
+
+clos.addEventListener("click", ()=>{
+    document.querySelector("#burg").style.display = "block";
+    document.querySelector(".nav").style.display = "none";
+    document.querySelector(".navigation").style.height = "auto";
+    document.querySelector(".close").style.display = "none";
+    console.log("check close")
+})
+
+
 // For Major and small projects tab
 
 let button = document.getElementById("majproj");
@@ -18,44 +42,36 @@ buttonn.addEventListener("click", () => {
 
 // for read more buttn
 
-let rd = document.getElementsByClassName("read-btn");
+// let rd = document.getElementsByClassName("read-btn");
 
-rd.addEventListener("click",()=>{
-    console.log("to check read btn")
-    document.querySelector(".read-more").style.display = "block";
-    document.querySelector("#read").style.display = "none";
-})
+// rd.addEventListener("click",()=>{
+//     console.log("to check read btn")
+//     document.querySelector(".read-more").style.display = "block";
+//     document.querySelector("#read").style.display = "none";
+// });
 
-function readd(btnNumber) {
-    console.log("to check read btn");
-    document.querySelector(`#read-more${btnNumber}`).style.display = "block";
-    document.querySelector(`#read${btnNumber}`).style.display = "none";
+// function readd(btnNumber) {
+//     console.log("to check read btn");
+//     document.querySelector(`#read-more${btnNumber}`).style.display = "block";
+//     document.querySelector(`#read${btnNumber}`).style.display = "none";
+// };
+
+
+let rdButtons = document.getElementsByClassName("read-btn");
+
+for (let i = 0; i < rdButtons.length; i++) {
+    rdButtons[i].addEventListener("click", function() {
+        console.log("to check read btn");
+
+        let readMore = this.parentElement.querySelector(".read-more");
+        let readBtn = this.parentElement.querySelector("#read");
+
+        if (readMore && readBtn) {
+            readMore.style.display = "block";
+            readBtn.style.display = "none";
+        }
+    });
 }
 
-// For Hamburger navigation
 
-// let burger = document.getElementById("burg");
 
-// burger.addEventListener("click", () => {
-//     console.log("burger clicked") //just to check
-//         document.querySelector(".nav").style.display = "block";
-// })
-
-let burger = document.getElementById("burg");
-let nav = document.querySelector(".nav");
-
-burger.addEventListener("click", () => {
-    console.log("burger clicked"); // Just for checking
-    nav.classList.toggle("show");
-});
-
-// let clos = document.getElementById("closed");
-
-// clos.addEventListener("click", ()=>{
-//     document.querySelector(".mob-nav1-ul").style.display = "none";
-//     document.querySelector(".nav-div").style.height = "auto";
-//     document.querySelector("#burg").style.display = "block";
-//     document.querySelector(".close").style.display = "none";
-//     document.querySelector(".banner-txt").style.position = "absolute";
-//     console.log("check close")
-// })
