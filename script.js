03fafb8f4d5636b23db5159406123cbd4139e757
Pritam Khaple <110626364+pritamkhaple@users.noreply.github.com>
2024-01-16 17:58:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -23,19 +23,32 @@ clos.addEventListener("click", ()=>{
 })
 
 // Get all list items inside the navigation
-let navItems = document.querySelectorAll('.nav-ul li');
+// let navItems = document.querySelectorAll('.nav-ul li');
 
 // Add click event listener to each list item
-navItems.forEach(item => {
-    item.addEventListener('click', () => {
-        // Hide the navigation
-        document.querySelector(".navv").style.display = "none";
-        document.querySelector(".navigation").style.height = "auto";
-        document.querySelector("#burg").style.display = "block";
-        document.querySelector(".close").style.display = "none";
-        document.querySelector("body").style.overflow = "auto";
+// navItems.forEach(item => {
+//     item.addEventListener('click', () => {
+//         document.querySelector(".navv").style.display = "none";
+//         document.querySelector(".navigation").style.height = "auto";
+//         document.querySelector("#burg").style.display = "block";
+//         document.querySelector(".close").style.display = "none";
+//         document.querySelector("body").style.overflow = "auto";
+//     });
+// });
+
+// Add click event listener to each list item only on mobile
+if (window.innerWidth <= 768) { // Adjust the breakpoint as needed
+    navItems.forEach(item => {
+        item.addEventListener('click', () => {
+            // Hide the navigation
+            document.querySelector(".navv").style.display = "none";
+            document.querySelector(".navigation").style.height = "auto";
+            document.querySelector("#burg").style.display = "block";
+            document.querySelector(".close").style.display = "none";
+            document.querySelector("body").style.overflow = "auto";
+        });
     });
-});
+}
 
 // For Major and small projects tab
 
